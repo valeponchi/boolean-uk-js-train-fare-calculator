b2d2e5f2d79955149df3175d5baa636e8c3de720
valeponchi <81304855+valeponchi@users.noreply.github.com>
2021-04-27 14:24:40 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let yourAge = Number(prompt("What is your age?"))
+let howFarYouTravelling = Number(prompt("How many Km far from the station do you need to go?"))
+let priceTicketPerKm = 0.21
+let travelPrice = howFarYouTravelling * priceTicketPerKm
+if (yourAge < 18) {
+  let discountMinors = (travelPrice / 100) * 80
+  alert("You can have a 20% discount!")
+  console.log("Total with 20% discount = " + ("£" + discountMinors.toFixed(2)))
+} else if (yourAge > 65) {
+  let discountSeniors = (travelPrice / 100) * 60
+  alert("You can have a 40% discount!")
+  console.log("Total with 40% discount = " + discountSeniors)  
+} else console.log("Total Travel Price = " + ("£" + travelPrice.toFixed(2)))
+
+// ("Mac USA-UK Index =" + Math.round(usaUkIndex * 100) + "%")
